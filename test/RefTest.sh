7f74246a17d3fe95c9910548ba6d896301c2d41a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-06-23 22:58:58 +0000

    make Ref test independent of POOL

---
+++ test/RefTest.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+Mange --parameter-set RefTest.txt
--- test/RefTest_t.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -f RefTest.root PoolCatalogFile.xml
-
-Mange --parameter-set RefTest_t.config
-
-Mange --parameter-set RefReadTest_t.config
