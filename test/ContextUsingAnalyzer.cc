5f13fdf097d9ecc8c48b2958a2e6ee290210ef72
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2005-07-07 21:54:11 +0000

    Beginnings of example EDAnalyzer, using seal::Context to pass semi-global data

---
+++ test/ContextUsingAnalyzer.cc
@@ -0,0 +1,54 @@
+#include "FWCore/FWCoreIntegration/test/ContextUsingAnalyzer.h"
+
+#include "SealKernel/ComponentWrapper.h"
+
+typedef seal::ComponentWrapper<const edm::Event> EventWrapper;
+
+InnerAlg::InnerAlg(const edm::ParameterSet& ps, seal::Context* common) :
+  myParam_(ps.getParameter<double>("myParam")),
+  context_(common)
+{ }
+
+void
+InnerAlg::doWork()
+{
+  // get the hidden products here, and do work with them.
+}
+
+
+// Note that when we contsruct the inner algorithm, we have to pass
+// along its parameter set, and a pointer to *our* context... I think.
+OuterAlg::OuterAlg(const edm::ParameterSet& ps, seal::Context* common) :
+  threshold_(ps.getParameter<int>("threshold")),
+  context_(common),
+  inner_(ps.getParameter<edm::ParameterSet>("inner"), &context_)
+{ }
+
+void
+OuterAlg::doWork()
+{
+  // get the hidden products here, and do work with them.
+}
+	    
+
+
+ContextUsingAnalyzer::ContextUsingAnalyzer( const  edm::ParameterSet& iConfig ) : 
+  context_()
+{ }
+
+
+ContextUsingAnalyzer::~ContextUsingAnalyzer()
+{ }
+
+
+void
+ContextUsingAnalyzer::analyze( const edm::Event& iEvent, const  edm::EventSetup& iSetup )
+{
+   using namespace edm;
+
+   //create the wrapper that will hold our Event
+   //  Note the 'magic' word "event"
+   new EventWrapper(&iEvent, &context_,"event");
+}
+
+DEFINE_FWK_MODULE(ContextUsingAnalyzer)
---
+++ test/ContextUsingAnalyzer.h
@@ -0,0 +1,61 @@
+#ifndef FWCORE_FWCOREINTEGRATION_CONTEXTUSINGANALYZER_H
+#define FWCORE_FWCOREINTEGRATION_CONTEXTUSINGANALYZER_H
+
+#include "FWCore/CoreFramework/interface/CoreFrameworkfwd.h"
+#include "FWCore/CoreFramework/interface/EDAnalyzer.h"
+
+#include "FWCore/CoreFramework/interface/Event.h"
+#include "FWCore/CoreFramework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SealKernel/Context.h"
+
+
+// ------------------------------------------------------------ 
+
+// We will create an analysis module (an EDAnalyzer) which contains an
+// algorithm object of type OuterAlg; OuterAlg will contain an
+// algorithm object of type InnerAlg.
+
+// These algorithm objects will not be passed the data they need in
+// order to do their work. Instead, those data will be hidden inside
+// the Context.
+
+class InnerAlg {
+ public:
+  InnerAlg(const edm::ParameterSet& ps, seal::Context* common);
+  void doWork(); // note lack of arguments
+
+ private:
+  double        myParam_;
+  seal::Context context_;
+};
+
+class OuterAlg {
+ public:
+  OuterAlg(const edm::ParameterSet& ps, seal::Context* common);
+  void doWork(); // note lack of arguments
+
+ private:
+  int           threshold_;
+  seal::Context context_;
+  InnerAlg      inner_;
+};
+
+
+class ContextUsingAnalyzer : public edm::EDAnalyzer 
+{
+ public:
+  explicit ContextUsingAnalyzer( const edm::ParameterSet& );
+  ~ContextUsingAnalyzer();
+  
+  virtual void analyze( const edm::Event&, const  edm::EventSetup& );
+
+ private:
+  
+  seal::Context context_;
+  
+};
+
+#endif
