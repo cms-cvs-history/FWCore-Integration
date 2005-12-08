78c19503e1b4cea40e956b8f2a2bd669ae43ce39
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-12-08 00:10:34 +0000

    test scripts must return cmsRun return value upon error.

--- test/context_using_example.sh
+++ test/context_using_example.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/context_using_example.cfg
+function die { echo $1: status $2 ;  exit $2; }
+
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/context_using_example.cfg || die 'Failed in context_using_example.cfg' $?
