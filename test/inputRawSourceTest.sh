c99e0209293f50df37e66777b2e9a05e5695abc0
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2008-06-30 21:51:55 +0000

    Convert to PYTHON, this finishes the conversion in FWCore/Integration package

--- test/inputRawSourceTest.sh
+++ test/inputRawSourceTest.sh
@@ -2,7 +2,7 @@
 
 function die { echo $1: status $2 ;  exit $2; }
 
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/inputRawSourceTest.cfg || die 'Failed in inputRawSourceTest.cfg' $?
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/inputRawSourceTest_cfg.py || die 'Failed in inputRawSourceTest_cfg.py' $?
 
 # Pass in name and status
 
